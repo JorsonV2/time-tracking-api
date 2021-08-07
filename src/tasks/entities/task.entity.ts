d626e79ba33573fc6f065aa7c4d31d79c09a9664
michalstyrylski <75220603+michalstyrylski@users.noreply.github.com>
2021-08-07 23:18:31 +0200

    define task entity

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,16 @@
-export class Task {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Task {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    start_date: Date;
+
+    @Column()
+    end_date: Date;
+}
