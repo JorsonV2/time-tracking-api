f1798cb193b7340f897aa81696833845416aefc7
michalstyrylski <75220603+michalstyrylski@users.noreply.github.com>
2021-08-08 00:19:14 +0200

    import typeorm module in database module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from 'src/tasks/entities/task.entity';
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        entities: [Task],
+        synchronize: true
+    })]
+})
 export class DatabaseModule {}
