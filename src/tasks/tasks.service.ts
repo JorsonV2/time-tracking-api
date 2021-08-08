cf6d6a06d22f2fbbc7f88c7e34c21894fe6500f8
michalstyrylski <75220603+michalstyrylski@users.noreply.github.com>
2021-08-08 06:57:59 +0200

    :art: switch for only using repository in tasks service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { throwError } from 'rxjs';
-import { Connection, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { StopTaskDto } from './dto/stop-task.dto';
 import { Task } from './entities/task.entity';
@@ -10,8 +9,7 @@ import { Task } from './entities/task.entity';
 export class TasksService {
   constructor(
     @InjectRepository(Task)
-    private tasksRepository: Repository<Task>,
-    private connection: Connection
+    private tasksRepository: Repository<Task>
   ){}
 
   async create(
@@ -23,38 +21,25 @@ export class TasksService {
     new_task.name = createTaskDto.name;
     new_task.start_date = new Date(createTaskDto.start_date);
 
-    const queryRunner = this.connection.createQueryRunner()
+    
 
-    await queryRunner.connect();
-    await queryRunner.startTransaction();
+    // check for previous not ended task
+    const active_task = await this.getActive();
 
-    try {
+    if (active_task) {
 
-      // check for previous not ended task
-      const active_task = await this.getActive();
+      //throw error when new tasks start time is before start time of active task
+      if (new_task.start_date <= active_task.start_date)
+        throw new Error(`New task can't start before or at the same time as active task.`)
+      
+      active_task.end_date = new_task.start_date;
 
-      if (active_task) {
-
-        //throw error when new tasks start time is before start time of active task
-        if (new_task.start_date <= active_task.start_date)
-          throw new Error(`New task can't start before or at the same time as active task.`)
-        
-        active_task.end_date = createTaskDto.start_date;
-
-        await queryRunner.manager.save(active_task);
-      }
-
-      await queryRunner.manager.save(new_task)
-
-      await queryRunner.commitTransaction();
+      await this.tasksRepository.save(active_task);
     }
-    catch (err) {
-      await queryRunner.rollbackTransaction();
-      console.log(err);
-    }
-    finally {
-      await queryRunner.release();
-    }
+
+    const created_task = this.tasksRepository.create(new_task);
+
+    return await this.tasksRepository.save(created_task);
   }
 
   async getActive() {
@@ -66,7 +51,6 @@ export class TasksService {
     });
 
     // check for the last startet task. If there is none or it has ended return null. Otherwhise return that task.
-
     if (lastTask[0])
       if (lastTask[0].end_date == null)
         return lastTask[0];
@@ -80,31 +64,18 @@ export class TasksService {
     stopTaskDto: StopTaskDto
   ) {
     const active_task = await this.getActive();
+
     if (active_task) {
-      const queryRunner = this.connection.createQueryRunner();
-
-      await queryRunner.connect();
-      await queryRunner.startTransaction();
-
       active_task.end_date = new Date(stopTaskDto.end_date);
 
-      try {
+      //throw error when new tasks start time is before start time of active task
+      if (active_task.start_date >= active_task.end_date)
+        throw new Error(`Tasks end date can't be before or at the same time as the start date.`)
 
-        //throw error when new tasks start time is before start time of active task
-        if (active_task.start_date >= active_task.end_date)
-          throw new Error(`End date can't end before or at the same time as it starts.`)
+      return await this.tasksRepository.save(active_task)
 
-        await queryRunner.manager.save(active_task)
-
-        await queryRunner.commitTransaction();
-      }
-      catch (err) {
-        await queryRunner.rollbackTransaction();
-        console.log(err);
-      }
-      finally {
-        await queryRunner.release();
-      }
     }
+      
+    return null;
   }
 }
