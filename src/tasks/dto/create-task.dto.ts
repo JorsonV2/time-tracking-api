8e4a45f3e7d1eaa5bc89f810501243fc25cb5a9d
michalstyrylski <75220603+michalstyrylski@users.noreply.github.com>
2021-08-08 03:00:12 +0200

    define CreateTaskDto with class validation and validation pipe

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,14 @@
-export class CreateTaskDto {}
+import { strict } from "assert/strict";
+import { IsDate, IsDateString, IsString } from "class-validator";
+
+export class CreateTaskDto {
+    @IsString({
+        message: "Name must be a string."
+    })
+    name: string;
+
+    @IsDateString({
+        strict: true
+    })
+    start_date: Date;
+}
